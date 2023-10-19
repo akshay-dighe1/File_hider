b59978ad5f5637214432953ca39740306f52ada2
akshay-dighe1 <135417318+akshay-dighe1@users.noreply.github.com>
2023-10-19 10:23:23 +0530

    Update SendOTPService.java

--- src/main/java/service/SendOTPService.java
+++ src/main/java/service/SendOTPService.java
@@ -15,23 +15,23 @@ public class SendOTPService {
         // Recipient's email ID needs to be mentioned.
         String to = email;
 
-        // Sender's email ID needs to be mentioned
+       
        final String from = "akshaydighe6696@gmail.com";
 
-//        Assuming you are sending email from through 
+
 
         String host = "smtp.gmail.com";
 
-        // Get system properties
+        
         Properties properties = System.getProperties();
 
-        // Setup mail server
+       
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-        // Get the Session object.// and pass u & p
+     
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -42,27 +42,27 @@ public class SendOTPService {
 
         });
 
-        // Used to debug SMTP issues
+       
         session.setDebug(true);
 
         try {
-            // Create a default MimeMessage object.
+           
             MimeMessage message = new MimeMessage(session);
 
-            // Set From: header field of the header.
+       
             message.setFrom(new InternetAddress(from));
 
-            // Set To: header field of the header.
+        
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-            // Set Subject: header field
+         
             message.setSubject("File Enc ka OTP");
 
-            // Now set the actual message
+           
             message.setText("Your One time Password for File Enc app is " + genOTP);
 
             System.out.println("sending...");
-            // Send message
+            
             Transport.send(message);
             System.out.println("Sent message successfully....");
         } catch (MessagingException mex) {
